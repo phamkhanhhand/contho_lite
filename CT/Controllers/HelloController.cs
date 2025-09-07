fd582d48cdddda342579760260e3fd9c4be9cead
Pham Khanh Ha <phamkhanhhand@gmail.com>
2025-09-07 16:51:00 +0700

    add project

---
+++ CT/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new[] {
+            new { Id = 1, Name = "Bàn phím" },
+            new { Id = 2, Name = "Chuột" }
+        });
+        }
+    }
+}
